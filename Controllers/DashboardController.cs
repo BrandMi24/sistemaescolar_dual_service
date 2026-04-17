0c71c928e09dc974e896fee327280700793ad39b
Efren <efrenlopez.dev@outlooj.com>
2026-04-17 07:44:06 -0500

    feat: update dashboard components, button adition to save all data into a zip folder

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ControlEscolar.Services; // Asegúrate de que apunte a donde está IDashboardService
+using ControlEscolar.Services;
 using ControlEscolar.Models.Dashboard;
 
 namespace ControlEscolar.Controllers
 {
-    // [Authorize] // Agrégalo cuando quieras activar la seguridad
     public class DashboardController : Controller
     {
         private readonly IDashboardService _dashboardService;
@@ -14,42 +13,36 @@ namespace ControlEscolar.Controllers
             _dashboardService = dashboardService;
         }
 
-        // 1. Inicio (Visión Rectoría)
         public async Task<IActionResult> Index(int? year, int? cuatrimestre)
         {
             var model = await _dashboardService.GetRectorateDataAsync(year, cuatrimestre);
             return View(model);
         }
 
-        // 2. Inscripciones
         public async Task<IActionResult> Admisiones(int? year, int? cuatrimestre)
         {
             var model = await _dashboardService.GetAdmissionsDataAsync(year, cuatrimestre);
             return View(model);
         }
 
-        // 3. Trámites
         public async Task<IActionResult> Tramites(int? year, int? cuatrimestre)
         {
             var model = await _dashboardService.GetTramitesDataAsync(year, cuatrimestre);
             return View(model);
         }
 
-        // 4. Vinculación
         public async Task<IActionResult> Vinculacion(int? year, int? cuatrimestre)
         {
             var model = await _dashboardService.GetVinculacionDataAsync(year, cuatrimestre);
             return View(model);
         }
 
-        // 5. Calidad Académica
         public async Task<IActionResult> Calidad(int? year, int? cuatrimestre)
         {
             var model = await _dashboardService.GetAcademicQualityDataAsync(year, cuatrimestre);
             return View(model);
         }
 
-        // 6. Servicios Médicos
         public async Task<IActionResult> Salud(int? year, int? cuatrimestre)
         {
             var model = await _dashboardService.GetMedicalDataAsync(year, cuatrimestre);
